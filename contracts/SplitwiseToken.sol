a83eafd3a02278ae51c2d5071ad13d785b4d5c2d
joanamonteiro1809 <jco.monteiro@campus.fct.unl.pt>
2025-06-05 14:56:19 +0200

    clean splitwiseToken.sol

--- contracts/SplitwiseToken.sol
+++ contracts/SplitwiseToken.sol
@@ -4,13 +4,9 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SplitwiseToken is ERC20 {
-    uint256 public constant TOKENS_PER_ETH = 1000; // Set your desired rate here
+    uint256 public constant TOKENS_PER_ETH = 1000; 
 
     constructor() ERC20("Trust Token", "TRUST") {}
-
-    /*function mint(address to, uint256 amount) public {
-        _mint(to, amount);
-    }*/
 
     function mintWithETH() external payable {
         require(msg.value > 0, "Send ETH to mint tokens");
